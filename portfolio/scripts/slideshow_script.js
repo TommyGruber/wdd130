91cce0aae119f25c52999ae05a2db665aed0315a
TommyGruber <gru21003@byui.edu>
2022-12-10 23:45:50 -0700

    Fixed slide show, updated 3rd page

--- portfolio/scripts/slideshow_script.js
+++ portfolio/scripts/slideshow_script.js
@@ -1,17 +1,14 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+let slideIndex = 0;
+showSlides();
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
-  x[slideIndex-1].style.display = "block";
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
